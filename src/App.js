b874836764f6ef235e9232d8bc290f48b542e603
Stsv <stsv77@yandex.ru>
2021-08-27 13:05:45 +0500

    get margin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import {presetGpnDefault, Theme} from '@consta/uikit/Theme';
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Header from './pages/Header/Header';
 import Footer from './pages/Footer/Footer';
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Dashboard/>
-            <Footer/>
-        </div>
+        <Theme preset={presetGpnDefault}>
+            <div>
+                <Header/>
+                <Dashboard/>
+                <Footer/>
+            </div>
+        </Theme>
     );
 };
 
